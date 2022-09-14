3742bf13443077a0096c48c0d291a13bbdf53a4d
Samiul Islam Mugdha <67468157+mugdha-samiul@users.noreply.github.com>
2022-09-15 02:50:18 +0600

    Update Extended_Euclidean.cpp

--- GCD/Extended_Euclidean.cpp
+++ GCD/Extended_Euclidean.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll gcdExtended(ll a, ll b, ll *x, ll *y) {
-   if(a == 0){
-      *x = 0;
-      *y = 1;
-      return b;
-   }
-   ll x1, y1;
-   ll gcd = gcdExtended(b%a, a, &x1, &y1);
-   *x = y1 - (b/a) * x1;
-   *y = x1;
-   return gcd;
+ll x, y, d;
+ll gcdExtended(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    d = gcdExtended(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
 }
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
-    ll x, y, a, b, d;
+    ll a, b;
     cin >> a >> b;
-    d = gcdExtended(a, b, &x, &y);
+    d = gcdExtended(a, b, x, y);
     cout << x << " " << y << " " << d << endl;
     return 0;
 }
